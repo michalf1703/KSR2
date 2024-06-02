fc1c7ffddba74bef1a5f2228184925178483213a
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2024-06-02 22:07:01 +0200

    repair database

--- KSR-project2/src/main/java/ksr2/ksrproject2/main.java
+++ KSR-project2/src/main/java/ksr2/ksrproject2/main.java
@@ -1,12 +1,16 @@
 package ksr2.ksrproject2;
 
-import ksr2.ksrproject2.logic.calculation.membershipFunctions.TrapezoidalFunction;
+import ksr2.ksrproject2.logic.utilis.DetalisReader;
+import ksr2.ksrproject2.logic.model.PowerliftingResult;
+
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
-        TrapezoidalFunction function = new TrapezoidalFunction(6, 6, 16, 23);
-        TrapezoidalFunctionPlot plot = new TrapezoidalFunctionPlot(function);
-        plot.pack();
-        plot.setVisible(true);
+        List<PowerliftingResult> results = DetalisReader.readData();
+
+        for (PowerliftingResult result : results) {
+            System.out.println(result.toString());
+        }
     }
 }
